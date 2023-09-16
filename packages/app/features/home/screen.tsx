59088e928e6ff2567ac432726c96079eb5f56152
Johnathan Simeroth <john.simeroth@gmail.com>
2023-09-16 18:08:24 +0100

    added external links

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,17 +1,23 @@
 import { ScrollView, XStack, YStack, Theme, ColorModePreview, SizableText, Separator } from '@my/ui'
-
 import { Menu } from '@tamagui/lucide-icons'
+import { Link } from 'solito/link'
 
 function Header() {
   return (
     <XStack jc='space-between' paddingVertical='$3' paddingHorizontal='$5'>
       <Menu />
       <XStack space='$3'>
-        <SizableText>GitHub</SizableText>
+        <Link href='https://github.com/johnsimeroth/learn-tamagui'>
+          <SizableText>GitHub</SizableText>
+        </Link>
         <Separator alignSelf="stretch" vertical />
-        <SizableText>Tamagui Docs</SizableText>
+        <Link href='https://tamagui.dev'>
+          <SizableText>Tamagui Docs</SizableText>
+        </Link>
         <Separator alignSelf="stretch" vertical />
-        <SizableText>Solito Docs</SizableText>
+        <Link href='https://solito.dev'>
+          <SizableText>Solito Docs</SizableText>
+        </Link>
       </XStack>
     </XStack>
   )
