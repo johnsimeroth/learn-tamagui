eba5f27da93fcbf9853311089bb1ce3b4a79c0d6
Johnathan Simeroth <john.simeroth@gmail.com>
2023-10-10 18:35:42 +0200

    start filling out .mdx

--- packages/app/features/lessons/lesson0.tsx
+++ packages/app/features/lessons/lesson0.tsx
@@ -1,5 +1,15 @@
-import { ScrollView, XStack, YStack, H1, MyHeader, NavMenu } from '@my/ui'
+import { ScrollView, XStack, YStack, H1, Paragraph, MyHeader, NavMenu } from '@my/ui'
 import { useState } from 'react'
+import { Lesson0MD } from './data/Lesson0MD';
+
+{`
+            You're on lesson 0: setup. By the end of this lesson you should:
+            1. Have a working web app that renders hello world,
+            2. Understand the high level folder structure of a tamagui/solito/expo monorepo, and
+            3. Know where to look for more information and details.
+
+            Let's jump right in
+            `}
 
 type Visible = boolean;
 
@@ -17,6 +27,7 @@ export function Lesson0() {
         <NavMenu isVisible={isVisible} />
         <ScrollView tag='main'>
           <H1>Lesson 0!</H1>
+          {Lesson0MD}
         </ScrollView>
       </XStack>
     </YStack>
